6359e440b47ace45c9e6197a426af1166d6bd2d6
oyefimova <oyefimova@kse.org.ua>
2024-10-23 18:46:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def get_student():
-    pass
+    students={}
+    for i in range(10):
+        name=input("Enter student's name")
+        grades=input('Enter grades')
+        final=input('Enter finals result')
+        students[name]={'grades': grades, 'final': final}
+    print(students)
 def academ_integrity():
     pass
 def start_program():
